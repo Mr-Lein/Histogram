56af36be20c91cd93a7bfd5ff3633358c15d59c4
Mr-Lein <151097135+Mr-Lein@users.noreply.github.com>
2024-09-24 11:28:02 -0600

    Update HistoIRunner.java

--- HistoIRunner.java
+++ HistoIRunner.java
@@ -14,15 +14,15 @@ public class HistoIRunner
 	public static void main( String args[] ) throws IOException
 	{
 		Histogram test = new Histogram("a b c d e f g h i a c d e g h i h k");
-		out.println(test);
+		test.printMap();
 
-		test.setSentence("1 2 3 4 5 6 1 2 3 4 5 1 3 1 2 3 4");
-		out.println(test);
+		Histogram test = new Histogram("1 2 3 4 5 6 1 2 3 4 5 1 3 1 2 3 4");
+		test.printMap();
 
-		test.setSentence("Y U I O Q W E R T Y");
-		out.println(test);
+		Histogram test = new Histogram("Y U I O Q W E R T Y");
+		test.printMap();
 
-		test.setSentence("4 T # @ ^ # # #");
-		out.println(test);
+		Histogram test = new Histogram("4 T # @ ^ # # #");
+		test.printMap();
 	}
 }
